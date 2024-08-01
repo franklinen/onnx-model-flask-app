0724fee7133ab7e59a59c52c3e74e7d69a6c2368
Frankline Ononiwu <franklinen.ononiwu@gmail.com>
2024-08-01 15:19:37 -0400

    Update styletransfer.py

--- styletransfer.py
+++ styletransfer.py
@@ -1,35 +1,48 @@
 from PIL import Image
 import numpy as np
 import onnxruntime as rt
-from flask import request
+from flask import request, Flask
 
 
 def styletransferProcess(directoryName, filename, selected_style):
+    # check if the post request has the file part
+    if 'imagefile' not in request.files:
+        return 'No file part'
+        
     imagefile= request.files['imagefile']
-    image_path = "./images/" + imagefile.filename
-    imagefile.save(image_path)
-
-    image = Image.open()
-    size_reduction_factor = 1 
-    image = image.resize((224,224), Image.ANTIALIAS)
-
-    # Preprocess image
-    x = np.array(image).astype('float32')
-    x = np.transpose(x, [2, 0, 1])
-    x = np.expand_dims(x, axis=0)
-
-    # model inference
-    session = rt.InferenceSession("./mosaic-9.onnx")
-    output_name = session.get_outputs()[0].name
-    input_name = session.get_inputs()[0].name
-    result = session.run([output_name], {input_name: x})[0][0]
-
-    # postprocess
-    result = np.clip(result, 0, 255)
-    result = result.transpose(1,2,0).astype("uint8")
-    imag = Image.fromarray(result).show()
     
-    return imag
+    if imagefile.filename == '':
+        return 'No selected file'
+        
+    if imagefile:
+        image_path = "./images/" + imagefile.filename
+        imagefile.save(image_path)
+
+        image = Image.open(image_path)
+        size_reduction_factor = 1 
+        image = image.resize((224,224), Image.LANCZOS)
+
+        # Preprocess image
+        x = np.array(image).astype('float32')
+        x = np.transpose(x, [2, 0, 1])
+        x = np.expand_dims(x, axis=0)
+
+        # Model inference
+        session = rt.InferenceSession("./mosaic-9.onnx")
+        output_name = session.get_outputs()[0].name
+        input_name = session.get_inputs()[0].name
+        result = session.run([output_name], {input_name: x})[0][0]
+
+        # postprocess
+        result = np.clip(result, 0, 255)
+        result = result.transpose(1,2,0).astype("uint8")
+        imag = Image.fromarray(result).show()
+        output_path = "./images/styled_" + imagefile.filename
+        imag.save(output_path)
+    
+        return f"Image saved at {output_path}"
+        
+    return 'Error processing the image'
 
 
 if __name__ == '__main__':
